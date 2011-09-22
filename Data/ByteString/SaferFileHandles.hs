324152493ef1512b79dd6c5396867d160366fe77
Bas van Dijk <v.dijk.bas@gmail.com>
2011-09-22 21:51:39 +0200

    Add hGetSome and hPutNonBlocking to Data.ByteString.SaferFileHandles

--- Data/ByteString/SaferFileHandles.hs
+++ Data/ByteString/SaferFileHandles.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE UnicodeSyntax, NoImplicitPrelude #-}
+{-# LANGUAGE CPP, UnicodeSyntax, NoImplicitPrelude #-}
 
 -------------------------------------------------------------------------------
 -- |
@@ -15,9 +15,15 @@ module Data.ByteString.SaferFileHandles
     ( hGetLine
     , hGetContents
     , hGet
+#if MIN_VERSION_explicit_iomodes_bytestring(0,2,0)
+    , hGetSome
+#endif
     , hGetNonBlocking
 
     , hPut
+#if MIN_VERSION_explicit_iomodes_bytestring(0,2,0)
+    , hPutNonBlocking
+#endif
     , hPutStr
     ) where
 
@@ -41,9 +47,15 @@ import Control.Monad.Trans.Region ( AncestorRegion )
 import qualified Data.ByteString.ExplicitIOModes as E ( hGetLine
                                                       , hGetContents
                                                       , hGet
+#if MIN_VERSION_explicit_iomodes_bytestring(0,2,0)
+                                                      , hGetSome
+#endif
                                                       , hGetNonBlocking
 
                                                       , hPut
+#if MIN_VERSION_explicit_iomodes_bytestring(0,2,0)
+                                                      , hPutNonBlocking
+#endif
                                                       , hPutStr
                                                       )
 
@@ -77,6 +89,15 @@ hGet ∷ ( FileHandle handle, ReadModes ioMode
      ⇒ handle ioMode pr → Int → cr B.ByteString
 hGet = wrap2 E.hGet
 
+#if MIN_VERSION_explicit_iomodes_bytestring(0,2,0)
+-- | Wraps @Data.ByteString.@'B.hGetSome'.
+hGetSome ∷ ( FileHandle handle, ReadModes ioMode
+           , pr `AncestorRegion` cr, MonadIO cr
+           )
+         ⇒ handle ioMode pr → Int → cr B.ByteString
+hGetSome = wrap2 E.hGetSome
+#endif
+
 -- | Wraps: @Data.ByteString.'B.hGetNonBlocking'@.
 hGetNonBlocking ∷ ( FileHandle handle, ReadModes ioMode
                   , pr `AncestorRegion` cr, MonadIO cr
@@ -90,6 +111,15 @@ hPut ∷ ( FileHandle handle, WriteModes ioMode
        )
      ⇒ handle ioMode pr → B.ByteString → cr ()
 hPut = wrap2 E.hPut
+
+#if MIN_VERSION_explicit_iomodes_bytestring(0,2,0)
+-- | Wraps @Data.ByteString.@'B.hPutNonBlocking'.
+hPutNonBlocking ∷ ( FileHandle handle, WriteModes ioMode
+                  , pr `AncestorRegion` cr, MonadIO cr
+                  )
+                ⇒ handle ioMode pr → B.ByteString → cr B.ByteString
+hPutNonBlocking = wrap2 E.hPutNonBlocking
+#endif
 
 -- | Wraps: @Data.ByteString.'B.hPutStr'@.
 hPutStr ∷ ( FileHandle handle, WriteModes ioMode
